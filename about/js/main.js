53d85f55d60bc433a5b357d3760326dd6f411413
Roy <szsplyr@163.com>
2020-03-09 13:42:30 +0800

    Site updated: 2020-03-09 13:42:29

--- about/js/main.js
+++ about/js/main.js
@@ -62,28 +62,28 @@ var viewHeight=function(){
 
 	}
 }
-var waited_develop=function(){
-    var waitedli=document.getElementsByClassName("nav_bar_ul")[0].children;
-    // var waiteda=waitedli.children;
-    console.log(waitedli);
-    // console.log(waiteda);
+// var waited_develop=function(){
+//     var waitedli=document.getElementsByClassName("nav_bar_ul")[0].children;
+//     // var waiteda=waitedli.children;
+//     console.log(waitedli);
+//     // console.log(waiteda);
     
-    for (var i=1;i<waitedli.length;i++){
-        if(waitedli[i].addEventListener){
-            waitedli[i].addEventListener("click",myfunction,false);
-                function myfunction(){
-            alert("waiting");
-        }
+//     for (var i=1;i<waitedli.length;i++){
+//         if(waitedli[i].addEventListener){
+//             waitedli[i].addEventListener("click",myfunction,false);
+//                 function myfunction(){
+//             alert("waiting");
+//         }
 
-        }
-        if(waitedli[i].attachEvent){
-            waitedli[i].attachEvent("onclick",myfunction); 
-            function myfunction(){
-                alert("waiting");
-            }
-        }
-    }
-}
+//         }
+//         if(waitedli[i].attachEvent){
+//             waitedli[i].attachEvent("onclick",myfunction); 
+//             function myfunction(){
+//                 alert("waiting");
+//             }
+//         }
+//     }
+// }
 var tagSwitch=function() 
 {
 	var navP=document.getElementById("nav_bar");
@@ -116,151 +116,6 @@ var tagSwitch=function()
 
     	}
     }
-}
-// html5--canvas
-var canvashtml5 = document.getElementById("html5").getContext("2d");
-var canvascss3=document.getElementById("css3").getContext("2d");
-var canvasjs=document.getElementById("js").getContext("2d");
-var canvasjq=document.getElementById("jq").getContext("2d");
-var canvasbs=document.getElementById("bs").getContext("2d");
-var canvasps=document.getElementById("ps").getContext("2d");
-var canwidth=160;
-var radius=70;
-var canhtml5=function(){
-	var deg = 0;
-    var html5T = function(deg){
-        var r = deg*Math.PI/180;   
-        canvashtml5.clearRect(0,0,canwidth,canwidth);   
-        canvashtml5.beginPath();  
-        canvashtml5.strokeStyle = "#F1652A"; 
-        canvashtml5.lineWidth = 10; 
-        canvashtml5.arc(canwidth/2,canwidth/2,radius,0-1/2*Math.PI,r-1/2*Math.PI,false); //
-        canvashtml5.stroke();
-    };
-    var time = setInterval(function(){
-    	var con=document.getElementsByTagName("section")[2];
-    	if (con.style.display=="block") {
-    		deg+=4;
-	        html5T(deg);
-	        if(deg>324){
-	            clearInterval(time);
-	        }
-	    }    
-    },20);
-}
-var cancss3=function(){
-	var deg = 0;
-    var css3T = function(deg){
-        var r = deg*Math.PI/180;   
-       	canvascss3.clearRect(0,0,canwidth,canwidth);   
-        canvascss3.beginPath();  
-        canvascss3.strokeStyle = "#409AD8"; 
-        canvascss3.lineWidth = 10; 
-       	canvascss3.arc(canwidth/2,canwidth/2,radius,0-1/2*Math.PI,r-1/2*Math.PI,false); //
-        canvascss3.stroke();
-    };
-    var time = setInterval(function(){
-    	var con=document.getElementsByTagName("section")[2];
-    	if (con.style.display=="block") {
-    		deg+=4;
-	        css3T(deg);
-	        if(deg>324){
-	            clearInterval(time);
-	        }
-	    }    
-    },20);
-}
-var canvasTotal=function(){
-
-}
-var canjs=function(){
-	var deg = 0;
-    var jsT = function(deg){
-        var r = deg*Math.PI/180;   
-        canvasjs.clearRect(0,0,canwidth,canwidth);   
-        canvasjs.beginPath();  
-        canvasjs.strokeStyle = "#ECB12C"; 
-        canvasjs.lineWidth = 10; 
-        canvasjs.arc(canwidth/2,canwidth/2,radius,0-1/2*Math.PI,r-1/2*Math.PI,false); //
-        canvasjs.stroke();
-    };
-    var time = setInterval(function(){
-    	var con=document.getElementsByTagName("section")[2];
-    	if (con.style.display=="block") {
-    		deg+=4;
-	        jsT(deg);
-	        if(deg>252){
-	            clearInterval(time);
-	        }
-	    }    
-    },20);
-}
-var canjq=function(){
-	var deg = 0;
-    var jqT = function(deg){
-        var r = deg*Math.PI/180;   
-        canvasjq.clearRect(0,0,canwidth,canwidth);   
-        canvasjq.beginPath();  
-        canvasjq.strokeStyle = "#75B143"; 
-        canvasjq.lineWidth = 10; 
-        canvasjq.arc(canwidth/2,canwidth/2,radius,0-1/2*Math.PI,r-1/2*Math.PI,false); //
-        canvasjq.stroke();
-    };
-    var time = setInterval(function(){
-    	var con=document.getElementsByTagName("section")[2];
-    	if (con.style.display=="block") {
-    		deg+=4;
-	        jqT(deg);
-	        if(deg>288){
-	            clearInterval(time);
-	        }
-	    }    
-    },20);
-}
-var canbs=function(){
-	var deg = 0;
-    var bsT = function(deg){
-        var r = deg*Math.PI/180;   
-        canvasbs.clearRect(0,0,canwidth,canwidth);   
-        canvasbs.beginPath();  
-        canvasbs.strokeStyle = "#583F85"; 
-        canvasbs.lineWidth = 10; 
-        canvasbs.arc(canwidth/2,canwidth/2,radius,0-1/2*Math.PI,r-1/2*Math.PI,false); //
-        canvasbs.stroke();
-    };
-    var time = setInterval(function(){
-    	var con=document.getElementsByTagName("section")[2];
-    	if (con.style.display=="block") {
-    		deg+=4;
-	        bsT(deg);
-	        if(deg>288){
-	            clearInterval(time);
-	        }
-	    }    
-    },20);
-}
-
-var ps=function(){
-	var deg = 0;
-    var psT = function(deg){
-        var r = deg*Math.PI/180;   
-        canvasps.clearRect(0,0,canwidth,canwidth);   
-        canvasps.beginPath();  
-        canvasps.strokeStyle = "#031A3A"; 
-        canvasps.lineWidth = 10; 
-        canvasps.arc(canwidth/2,canwidth/2,radius,0-1/2*Math.PI,r-1/2*Math.PI,false); //
-        canvasps.stroke();
-    };
-    var time = setInterval(function(){
-    	var con=document.getElementsByTagName("section")[2];
-    	if (con.style.display=="block") {
-    		deg+=4;
-	        psT(deg);
-	        if(deg>288){
-	            clearInterval(time);
-	        }
-	    }    
-    },20);
 }
 
 //judge
@@ -319,101 +174,86 @@ var checkCon=function(){
 	}
 }
 
-var usernameon=document.getElementById("username");
-usernameon.onblur=checkUsername;
-
-var mymailon=document.getElementById("usermail");
-mymailon.onblur=checkMail;
-
-var mythemeon=document.getElementById("usertheme");
-mythemeon.onblur=checkTheme;
-var myconon=document.getElementById("usercon");
-myconon.onblur=checkCon;
 
 
 
-var scrollFunc = function (e) {
-    var con=document.getElementsByTagName("section");
-        var tag=document.getElementsByTagName("aside")[0].children;  
+// var scrollFunc = function (e) {
+    // var con=document.getElementsByTagName("section");
+    //     var tag=document.getElementsByTagName("aside")[0].children;  
     
-    var direct = 0;
-    e = e || window.event;
-    if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件   
+    // var direct = 0;
+    // e = e || window.event;
+    // if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件   
           
-        if (e.wheelDelta > 0) { //当滑轮向上滚动时
-            for (var i = 1; i < tag.length; i++) {
-                if(tag[i].className=="cur_a"){
-                    tag[i].className="";
-                    con[i].style.display="none";
-                    tag[i-1].className="cur_a";
-                    con[i-1].style.display="block";
-                    break;
-                }
-            }
-        }
-        if (e.wheelDelta < 0) { //当滑轮向下滚动时
+    //     if (e.wheelDelta > 0) { //当滑轮向上滚动时
+    //         for (var i = 1; i < tag.length; i++) {
+    //             if(tag[i].className=="cur_a"){
+    //                 tag[i].className="";
+    //                 con[i].style.display="none";
+    //                 tag[i-1].className="cur_a";
+    //                 con[i-1].style.display="block";
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     if (e.wheelDelta < 0) { //当滑轮向下滚动时
             
-            for (var i = 0; i < tag.length-1; i++) {
-                var n=0;
-                if(tag[i].className=="cur_a"){
-                    tag[i].className="";
-                    con[i].style.display="none";  
-                    n=i+1;
-                    tag[n].className="cur_a";
-                    con[n].style.display="block";
-                    break;
-                }
-            }
+    //         for (var i = 0; i < tag.length-1; i++) {
+    //             var n=0;
+    //             if(tag[i].className=="cur_a"){
+    //                 tag[i].className="";
+    //                 con[i].style.display="none";  
+    //                 n=i+1;
+    //                 tag[n].className="cur_a";
+    //                 con[n].style.display="block";
+    //                 break;
+    //             }
+    //         }
 
-        }
-    } else if (e.detail) {  //Firefox滑轮事件
-        if (e.detail> 0) { //当滑轮向上滚动时
-            for (var i = 1; i < tag.length; i++) {
-                if(tag[i].className=="cur_a"){
-                    tag[i].className="";
-                    con[i].style.display="none";
-                    tag[i-1].className="cur_a";
-                    con[i-1].style.display="block";
-                    break;
-                }
-            }
-        }
-        if (e.detail< 0) { //当滑轮向下滚动时
-            for (var i = 0; i < tag.length-1; i++) {
-                var n=0;
-                if(tag[i].className=="cur_a"){
-                    tag[i].className="";
-                    con[i].style.display="none";  
-                    n=i+1;
-                    tag[n].className="cur_a";
-                    con[n].style.display="block";
-                    break;
-                }
-            }
-        }
-    }
+    //     }
+    // } else if (e.detail) {  //Firefox滑轮事件
+    //     if (e.detail> 0) { //当滑轮向上滚动时
+    //         for (var i = 1; i < tag.length; i++) {
+    //             if(tag[i].className=="cur_a"){
+    //                 tag[i].className="";
+    //                 con[i].style.display="none";
+    //                 tag[i-1].className="cur_a";
+    //                 con[i-1].style.display="block";
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     if (e.detail< 0) { //当滑轮向下滚动时
+    //         for (var i = 0; i < tag.length-1; i++) {
+    //             var n=0;
+    //             if(tag[i].className=="cur_a"){
+    //                 tag[i].className="";
+    //                 con[i].style.display="none";  
+    //                 n=i+1;
+    //                 tag[n].className="cur_a";
+    //                 con[n].style.display="block";
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
     // ScrollText(direct);
-}
+// }
+
 // 给页面绑定滑轮滚动事件
-if (document.addEventListener) {
-    document.addEventListener('DOMMouseScroll', scrollFunc, false);
-}
+// if (document.addEventListener) {
+    // document.addEventListener('DOMMouseScroll', scrollFunc, false);
+// }
 // //滚动滑轮触发scrollFunc方法
 
 //以下是调用上面的函数
-var browser = myBrowser();
-if ("IE" == browser) {
-    document.onmousewheel = scrollFunc;  
-}else{
-    window.onmousewheel= scrollFunc;  
-}
+// var browser = myBrowser();
+// if ("IE" == browser) {
+//     document.onmousewheel = scrollFunc;  
+// }else{
+//     window.onmousewheel= scrollFunc;  
+// }
 
 viewHeight();
 tagSwitch();
-canhtml5();
-cancss3();
-canjs();
-canjq();
-canbs();
-ps();
 waited_develop();
